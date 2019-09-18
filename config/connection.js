26574a2ff9885cf9f34eff33af5b7e6b520ec9ca
Martin Polder <martinpolder@gmail.com>
2019-09-18 11:50:08 -0400

    database & connection set. working on ORM(lolwtfisthis)

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "dak0!-MMP",
+    database: "burgers_db"
+
+})
+
+
+connection.connect(function(err) {
+    if(err) {
+        console.log("error connection to server" + err.stack);
+        return;
+    }
+    console.log("connected as id: " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name) VALUES ('Bison Burger');
