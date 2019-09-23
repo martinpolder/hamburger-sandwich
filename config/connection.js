13bfb6ea2e3cca225227f257a9aaba26c0e1fa9e
Martin Polder <martinpolder@gmail.com>
2019-09-23 13:17:53 -0400

    got it working, time to style

--- config/connection.js
+++ config/connection.js
@@ -13,11 +13,12 @@ var connection = mysql.createConnection({
 
 
 connection.connect(function(err) {
-    if(err) {
-        console.log("error connection to server" + err.stack);
-        return;
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
     }
-    console.log("connected as id: " + connection.threadId);
-});
+    console.log("connected as id " + connection.threadId);
+  });
+  
 
 module.exports = connection;
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,55 +2,60 @@ var express = require("express");
 
 var router = express.Router();
 
+// Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-
-//home URL rendering of burger data thru handlebars
-router.get("/", function(req, res){
-    burger.all(function(data){
-        var hbsObject = {
-            burgers: data
-        };
-        console.table(hbsObject);
-        res.render("index", hbsObject);
-    });
-
-
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-//posting new burgers to the api page
-router.post("/api/burgers", function(req, res){
-    burger.create(
-        "burger_name", req.body.burger_name,
-        function(result) {
-
-            res.json({id: result.insertId});
-        });
-    
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
 });
-
-
-//router to put burgers into 'devoured' table
 
 router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
-    burger.update({
-      devoured: req.body.devoured
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
-  
+});
 
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-//skip delete for now, not explicitly necessary
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
-  module.exports = router;
+// Export routes for server.js to use.
+module.exports = router;
