9f2bc50d605828cf8b4566e9947605c448bbc3b7
Martin Polder <martinpolder@gmail.com>
2019-09-24 12:52:13 -0400

    connection to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,9 @@ var mysql = require("mysql");
 
 
 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,8 +12,8 @@ var connection = mysql.createConnection({
     password: "dak0!-MMP",
     database: "burgers_db"
 
-})
-
+});
+};
 
 connection.connect(function(err) {
     if (err) {
